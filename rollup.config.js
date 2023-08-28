eed36b7fa84e24d3929bcc27c8fa355b99aa38f6
fkakatie <ksexton@adobe.com>
2023-08-28 14:58:14 -0400

    feat: setup tests, rollup

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import cleanup from 'rollup-plugin-cleanup';
+
+const banner = `/*
+ * Copyright ${new Date().getFullYear()} Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+`;
+
+const bundles = [
+  {
+    source: 'src/lib-franklin.js',
+    outputFile: 'dist/aem-lib',
+  },
+];
+
+export default [...bundles.map(({ outputFile, source }) => ({
+  input: source,
+  inlineDynamicImports: true,
+  output: [
+    {
+      file: `${outputFile}.js`,
+      format: 'es',
+      sourcemap: false,
+      exports: 'auto',
+      banner,
+    },
+  ].filter((m) => m),
+  plugins: [
+    cleanup({
+      comments: ['eslint', 'jsdoc', /^\//],
+      maxEmptyLines: -1,
+    }),
+  ],
+}))];
