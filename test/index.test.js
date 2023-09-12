f18976104f205b275c62642acddf39134dae832f
Alexandre Capt <acapt@adobe.com>
2023-09-12 11:43:17 +0200

    chore: test coverage to 100

---
+++ test/index.test.js
@@ -0,0 +1,3 @@
+// trick to load all functions and make sure test coverage includes everything
+// eslint-disable-next-line no-unused-vars
+import * as index from '../src/index.js';
---
+++ web-test-runner.config.js
@@ -0,0 +1,12 @@
+export default {
+  coverageConfig: {
+    report: true,
+    reportDir: 'coverage',
+    threshold: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+};
