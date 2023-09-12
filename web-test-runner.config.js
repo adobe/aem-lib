fa53374fc8c70ec949fb79788f51fa0fcab56b6d
Alexandre Capt <acapt@adobe.com>
2023-09-12 15:07:53 +0200

    feat: target test coverage 100 (#7)

---
+++ web-test-runner.config.js
@@ -0,0 +1,19 @@
+export default {
+  coverageConfig: {
+    report: true,
+    reportDir: 'coverage',
+    threshold: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+    exclude: [
+      'test/fixtures/**',
+      'node_modules/**',
+    ],
+  },
+  files: [
+    'test/**/*.test.{html,js}',
+  ],
+};
