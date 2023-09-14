e70c8a8b3b32ad606565464afce000b00f7d0d66
Alexandre Capt <acapt@adobe.com>
2023-09-14 14:23:10 +0200

    feat: release needs to contain latest aem.js

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -5,9 +5,12 @@ module.exports = {
     ["@semantic-release/changelog", {
       "changelogFile": "CHANGELOG.md",
     }],
+    ['@semantic-release/exec', {
+      prepareCmd: 'npm run build',
+    }],
     "@semantic-release/npm",
     ['@semantic-release/git', {
-      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', 'dist/aem.js'],
       message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
     ["@semantic-release/github", {}],
