c3f10bc48cef07be0f53176fea478e2c839afeb7
Lars Trieloff <lars@trieloff.net>
2023-09-14 12:57:30 +0200

    fix: do not use a subdirectory for aem-boilerplate

--- publish.sh
+++ publish.sh
@@ -8,15 +8,17 @@ TYPE=$2 # can be major, minor, patch
 VERSION_DASH=${VERSION//./-}
 BRANCH=update-lib-aem-$TYPE-$VERSION_DASH
 
+cd ..
+echo "Using gh version: $(gh --version)"
 echo gh repo clone $ORG/$REPO upstream
-gh repo clone $ORG/$REPO upstream
-cp dist/aem.js upstream/$FILENAME
+gh repo clone $ORG/$REPO
+cp aem-lib/dist/aem.js $REPO/$FILENAME
 
-cd upstream
+cd $REPO
 
-git remote remove origin
-git remote add origin https://${GITHUB_TOKEN}@github.com/$ORG/$REPO
-git push
+# git remote remove origin
+# git remote add origin https://${GITHUB_TOKEN}@github.com/$ORG/$REPO
+# git push
 
 git checkout -b $BRANCH
 git add $FILENAME
