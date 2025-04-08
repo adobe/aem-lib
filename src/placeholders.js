e4e3d6271702a12de78ee327064a4904b760db97
Carlos A. Cabrera <cacabrer@adobe.com>
2025-04-08 08:49:52 -0500

    rename fallback variable to merge for clarity

--- src/placeholders.js
+++ src/placeholders.js
@@ -20,14 +20,13 @@ import { getMetadata } from './dom-utils.js';
 // eslint-disable-next-line import/prefer-default-export
 export async function fetchPlaceholders(prefix = 'default') {
   const root = getMetadata('root')?.replace(/\/$/, '') || (prefix === 'default' ? '' : prefix);
-  const [override, fallback] = getMetadata('placeholders')?.split('\n') || [];
+  const [override, merge] = getMetadata('placeholders')?.split('\n') || [];
   const url = override || `${root}/placeholders.json`;
   window.placeholders = window.placeholders || {};
 
   if (!window.placeholders[prefix]) {
     window.placeholders[prefix] = new Promise((resolve) => {
-      // const url = fallback || `${prefix === 'default' ? '' : prefix}/placeholders.json`;
-      Promise.all([fetch(url), fallback ? fetch(fallback) : Promise.resolve()])
+      Promise.all([fetch(url), merge ? fetch(merge) : Promise.resolve()])
         // get json from sources
         .then(async ([resp, oResp]) => {
           if (resp.ok) {
@@ -39,19 +38,19 @@ export async function fetchPlaceholders(prefix = 'default') {
           return [{}];
         })
         // process json from sources
-        .then(([json, oJson]) => {
+        .then(([json, mJson]) => {
           const placeholders = {};
 
           const allKeys = new Set([
             ...(json.data?.map(({ Key }) => Key) || []),
-            ...(oJson?.data?.map(({ Key }) => Key) || []),
+            ...(mJson?.data?.map(({ Key }) => Key) || []),
           ]);
 
           allKeys.forEach((Key) => {
             if (!Key) return;
             const keys = Key.split('.');
             const originalValue = json.data?.find((item) => item.Key === Key)?.Text;
-            const overrideValue = oJson?.data?.find((item) => item.Key === Key)?.Text;
+            const overrideValue = mJson?.data?.find((item) => item.Key === Key)?.Text;
             const finalValue = overrideValue ?? originalValue;
             const lastKey = keys.pop();
             const target = keys.reduce((obj, key) => {
